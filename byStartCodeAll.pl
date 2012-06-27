d5018b3f072d47c108cf39e0d94b045e526087bf
lncd <lncd@skynet.local>
2012-06-27 12:05:13 -0400

    initial

---
+++ byStartCodeAll.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/env perl
+use v5.12.3;
+use strict;
+use warnings;
+use Spreadsheet::Read;
+
+say join("\t", qw(subj run start target trial count sacc score lat lat? acc acc? note logic));
+
+# get each spreadsheet
+for my $xlsfile (glob('/Volumes/Connor/bars/data/1*/eye_scoring/fs*.xls')){
+
+ # match subject and run number, extract
+ next unless $xlsfile =~ m:data/(\d+)/eye_scoring/fs.*(\d).xls:;
+ my $subj=$1;
+ my $run=$2;
+
+ say "\t$subj $run\n";
+
+ # read in xls file
+ say "reading $xlsfile";
+ my $wb = ReadData($xlsfile);
+ 
+ say "done";
+ last;
+
+ for my $cells ($wb->[1]{cell}) {
+  say join("\t",$subj,$run,@$cells),"\n" if $cells->[1] =~ m/50|100|200/ 
+                                         && $cells->[6]+$cells->[8] >=1;
+ }
+ last;
+}
+
+
+
---
+++ byStartCodeAll.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+
+#from openpyxl import load_workbook
+from xlrd import open_workbook
+import glob
+import re
+
+# header
+print "\t".join( ['subj', 'run', 'start', 'target', 'trial', 'count', 'sacc', 'score', 'lat', 'lat?', 'acc', 'acc?', 'note', 'logic']);
+
+# get each spreadsheet
+for xlsfile in glob.glob('/Volumes/Connor/bars/data/1*/eye_scoring/fs*.xls'):
+   
+   # load sheet and extract subject number
+   wb   = open_workbook(xlsfile)
+   subj = xlsfile.split('/')[5].split('/')[-1]
+   run  = xlsfile[-5]
+
+   ws = wb.sheet_by_name(u'Sheet1')
+
+   for rownum in range(ws.nrows):
+       if re.match('50|100|200', str(ws.cell(rownum,0).value ))  and  (ws.cell(rownum,7).value or ws.cell(rownum,9).value):
+          print "\t".join(map(str,[subj,run]+ws.row_values(rownum)))
+
