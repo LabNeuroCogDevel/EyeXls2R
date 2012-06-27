52281f176579da879a1de655b0dff880767dc1db
lncd <lncd@skynet.local>
2012-06-27 13:20:51 -0400

    more python like

--- byStartCodeAll.py
+++ byStartCodeAll.py
@@ -4,7 +4,6 @@
 #from openpyxl import load_workbook
 from xlrd import open_workbook
 import glob
-import re
 
 # header
 print "\t".join( ['subj', 'run', 'start', 'target', 'trial', 'count', 'sacc', 'score', 'lat', 'lat?', 'acc', 'acc?', 'note', 'logic']);
@@ -17,9 +16,8 @@ for xlsfile in glob.glob('/Volumes/Connor/bars/data/1*/eye_scoring/fs*.xls'):
    subj = xlsfile.split('/')[5].split('/')[-1]
    run  = xlsfile[-5]
 
-   ws = wb.sheet_by_name(u'Sheet1')
+   ws = wb.sheet_by_name('Sheet1')
 
    for rownum in range(ws.nrows):
-       if re.match('50|100|200', str(ws.cell(rownum,0).value ))  and  (ws.cell(rownum,7).value or ws.cell(rownum,9).value):
+       if ws.cell(rownum,0).value in (50,100,200)  and  (ws.cell(rownum,7).value or ws.cell(rownum,9).value):
           print "\t".join(map(str,[subj,run]+ws.row_values(rownum)))
-
